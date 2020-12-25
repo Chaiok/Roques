087cf96df53c553bfa188f943aa99267f4139080
Артур Шакура <shakura2marta@gmail.com>
2020-12-25 22:25:42 +0300

    синица пара

--- main_project/src/commands.clj
+++ main_project/src/commands.clj
@@ -19,43 +19,13 @@
 )
 
 (defn moving  []
-  (def YY)
-  (def XX)
   (dosync
-            (doseq [[kk vv] (commute player/streams get (str "player" player/*id* ":")) ] 
-              (if (= kk "y:")
-                (def YY vv)
-              )
-              (if (= kk "x:")
-                (def XX vv)
-              )
-            )
-            (doseq [[kk vv] (commute player/states get (str "player" player/*id* ":"))]
-              ;;up
-              (if (= kk :up)
-                (if (= vv true)
-                  (def YY (- YY 5))
-                )
-              )
-              ;;down
-              (if (= kk :down)
-                (if (= vv true)
-                  (def YY (+ YY 5))
-                )
-              )
-              ;;left
-              (if (= kk :left)
-                (if (= vv true)
-                  (def XX (- XX 5))
-                )
-              )
-              ;;right
-              (if (= kk :right)
-                (if (= vv true)
-                  (def XX (+ XX 5))
-                )
-              )
-            )
-            (commute player/streams assoc (str "player" player/*id* ":") {"x:" XX "y:" YY})
+    (let [playerid
+          x (get (@player/streams playerid) "x:")
+          y (get (@player/streams playerid) "y:")
+          x1 (inc x)
+          y1 (inc y)]
+      (commute player/streams assoc (str "player" player/*id* ":") {"x:" x1 "y:" y1})
+    )
   )
 )
