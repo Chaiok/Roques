848ba6a50bc67feaac03ccc2895769658242094e
Артур Шакура <shakura2marta@gmail.com>
2020-12-25 16:36:12 +0300

    Доработка движения

--- main_project/index.js
+++ main_project/index.js
@@ -14,11 +14,11 @@ app.get('/', function (req, res) {
 });
 //server
 var client = new net.Socket();
-client.connect(4444, 'localhost', function () {
+client.connect(4448, 'localhost', function () {
     console.log('Connected to server');
 })
 client.on('data', function (data) {
-    console.log('Reciived server: ' + data);
+    //console.log('Reciived server: ' + data);
     io.sockets.emit('state', { msg: data })
 })
 client.on('close', function () {
@@ -32,11 +32,11 @@ io.on('connection', function (socket) {
 
     var client = new net.Socket();
 
-    client.connect(3333, 'localhost', function () {
+    client.connect(3336, 'localhost', function () {
         console.log('Connected');
     })
     client.on('data', function (data) {
-        console.log('Reciived: ' + data);
+        //console.log('Reciived: ' + data);
         //io.sockets.emit('add mess', { msg: data }); //отправляет сообщение всей группе сокетов
         socket.emit('add mess', { msg: data }); //отправляет сообщение одному сокету
     })
@@ -46,67 +46,18 @@ io.on('connection', function (socket) {
     })
 
     socket.on('chat message', function (data) {
-        console.log('message: ' + data);
+        //console.log('message: ' + data);
         client.write(data + "\n");
     });
     socket.on('movement', function (data) {
-        console.log('message: ' + JSON.stringify(data));
+        //console.log('message: ' + JSON.stringify(data));
         client.write( JSON.stringify(data) + "\n");
     });
     socket.on('disconnect', function () {
         console.log('Socket close');
         client.destroy();
-
-        //game функция
-        //delete players[socket.id];
-        //delete stateplayers[socket.id];
     })
-    /*
-    //game функции
-    players[socket.id] = {
-        x: 300,
-        y: 300,
-    };
-    stateplayers[socket.id] = {
-        up: false,
-        down: false,
-        left: false,
-        right: false
-    };
-    socket.on('movement', function (data) {
-        stateplayers[socket.id].up = data.up;
-        stateplayers[socket.id].down = data.down;
-        stateplayers[socket.id].left = data.left;
-        stateplayers[socket.id].right = data.right;
-    });
-    */
 });
-/*
-//game
-//передает всем в game данные игрокам
-setInterval(function () {
-    io.sockets.emit('state', players);
-}, 1000 / 60);
-//отвечает за движение
-var lastUpdateTime = (new Date()).getTime();
-setInterval(function () {
-    var currentTime = (new Date()).getTime();
-    var timeDifference = currentTime - lastUpdateTime;
-    for (var id in players) {
-        var player = players[id];
-        var stateplayer = stateplayers[id];
-        if (stateplayer.up)
-            player.y -= 0.1 * timeDifference;
-        if (stateplayer.down)
-            player.y += 0.1 * timeDifference;
-        if (stateplayer.left)
-            player.x -= 0.1 * timeDifference;
-        if (stateplayer.right)
-            player.x += 0.1 * timeDifference;
-    }
-    lastUpdateTime = currentTime;
-}, 1000 / 60);
-*/
 
 server.listen(PORT, function (error) {
     if (error) {
