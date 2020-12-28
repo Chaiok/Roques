7a97dc497f41592ba45efa2870b5ba6fdee89b4f
Артур Шакура <shakura2marta@gmail.com>
2020-12-28 11:04:42 +0300

    Очистка лишних файлов исправление прошлых пулов

--- main_project/src/server.clj
+++ main_project/src/server.clj
@@ -5,8 +5,8 @@
           [commands :as commands]
           [clojure.string :as str]))
 
-(def port (* 3 1112))
-(def sideport (* 4 1112))
+(def port (* 3 1111))
+(def sideport (* 4 1111))
 (def i 1)
 
 (defn mire-handle-client [in out]
@@ -63,7 +63,7 @@
             *err* (io/writer System/err)]
     
     
-    (let [f (atom 1) i (atom 300) y1 (atom 600) y2 (atom 600) x1 (atom 800) x2 (atom 800)]
+    (let [f (atom 1) y1 (atom 600) y2 (atom 600) x1 (atom 800) x2 (atom 800)]
       (
        (
         (loop []
@@ -83,22 +83,18 @@
             (commands/stenka @x2 600 (str @x2 "h4")) 
             (reset! x2 (- @x2 20))
           )
-            (while (pos? @i)
-            (commands/spawnWalls 800 600 @i) 
-            (reset! i (- @i 20))
-          ) 
           
-         (dosync (print (commute player/streams merge nil)) (flush))   
-         (commands/movingall 10)
-         (commands/spawnred 800 600 @f)
-         (swap! f inc)
+          (dosync (print (commute player/streams merge nil)) (flush))   
+          (commands/movingall 10)
+          (commands/spawnred 800 600 @f)
+          (swap! f inc)
          
-         (if (= @f 150)
-           (reset! f 1)
-           )
-         (Thread/sleep 20) 
-         (recur)
-         )
+          (if (= @f 100)
+            (reset! f 1)
+            )
+          (Thread/sleep 20) 
+          (recur)
+          )
        )
        )
       )
